a7024d56d24e916549c63f57b435c69033665019
hoanphungt <hoanphung.ptso1@gmail.com>
2019-05-29 11:22:17 +0200

    Fix some css properties to ensure the responsiveness of the web

--- src/components/RasterList.tsx
+++ src/components/RasterList.tsx
@@ -53,13 +53,13 @@ class RasterList extends React.Component<MyProps, {}> {
                 <div className="raster-list__content">
                     <ul className="raster-list__list">
                         <li className="raster-list__row-title">
-                            <div className="raster-list__row-box" />
-                            <div className="raster-list__row-type">Type</div>
-                            <div className="raster-list__row-name">Name</div>
-                            <div className="raster-list__row-org">Organisation</div>
-                            <div className="raster-list__row-obs">Obs.Type</div>
-                            <div className="raster-list__row-time">Latest update</div>
-                            <div className="raster-list__row-acc" />
+                            <div className="raster-list__row raster-list__row-box" />
+                            <div className="raster-list__row raster-list__row-type">Type</div>
+                            <div className="raster-list__row raster-list__row-name">Name</div>
+                            <div className="raster-list__row raster-list__row-org">Organisation</div>
+                            <div className="raster-list__row raster-list__row-obs">Obs.Type</div>
+                            <div className="raster-list__row raster-list__row-time">Latest update</div>
+                            <div className="raster-list__row raster-list__row-acc" />
                         </li>
                         {rasters.map((raster: Raster) => {
                             //Here is a logic to define whether a raster has been selected (check-box has been checked or not)
@@ -69,14 +69,14 @@ class RasterList extends React.Component<MyProps, {}> {
                             const checked = rasterUuid ? true : false;
 
                             return (
-                                <li className="raster-list__row" key={raster.uuid} onClick={() => selectRaster(raster)} >
-                                    <input type="checkbox" className="raster-list__row-box" onClick={() => this.onCheckboxSelect(raster)} defaultChecked={checked} />
-                                    <div className="raster-list__row-type">#</div>
-                                    <div className="raster-list__row-name">{raster.name}</div>
-                                    <div className="raster-list__row-org">{raster.organisation.name}</div>
-                                    <div className="raster-list__row-obs">{raster.observation_type.parameter}</div>
-                                    <div className="raster-list__row-time">{new Date(raster.last_modified).toLocaleDateString()}</div>
-                                    <div className="raster-list__row-acc" />
+                                <li className="raster-list__row-li" key={raster.uuid} onClick={() => selectRaster(raster)} >
+                                    <input className="raster-list__row raster-list__row-box" type="checkbox" onClick={() => this.onCheckboxSelect(raster)} defaultChecked={checked} />
+                                    <div className="raster-list__row raster-list__row-type">#</div>
+                                    <div className="raster-list__row raster-list__row-name">{raster.name}</div>
+                                    <div className="raster-list__row raster-list__row-org">{raster.organisation.name}</div>
+                                    <div className="raster-list__row raster-list__row-obs">{raster.observation_type.parameter}</div>
+                                    <div className="raster-list__row raster-list__row-time">{new Date(raster.last_modified).toLocaleDateString()}</div>
+                                    <div className="raster-list__row raster-list__row-acc" />
                                 </li>
                             )
                         })}
