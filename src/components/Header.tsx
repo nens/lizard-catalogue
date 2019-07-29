13b8d482453bdc8fb2077acef38d0aefb12e5e45
hoanphungt <hoanphung.cpo@gmail.com>
2019-07-29 09:28:08 +0200

    Remove Organisation selection; Use basket icon; Change text in info modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ class Header extends React.Component<MyProps> {
                 <div className="header-nav">
                     <a href="#basket" className="header-nav__icon-box" title={`${basket.length} items in the basket`}>
                         <svg className="header-nav__icon">
-                            <use xlinkHref="image/symbols.svg#icon-cart" />
+                            <use xlinkHref="image/symbols.svg#icon-shopping-basket" />
                         </svg>
                         {basket.length === 0 ? <span /> : <span className="header-nav__notification">{basket.length}</span>}
                         <span className="header-nav__text">Basket</span>
@@ -67,12 +67,12 @@ class Header extends React.Component<MyProps> {
                         </svg>
                         <span className="header-nav__text">User</span>
                     </div>
-                    <div className="header-nav__icon-box">
+                    {/* <div className="header-nav__icon-box">
                         <svg className="header-nav__icon">
                             <use xlinkHref="image/symbols.svg#icon-sort" />
                         </svg>
                         <span className="header-nav__text">Nelen &amp; Schuurmans</span>
-                    </div>
+                    </div> */}
                     <a href="#information" className="header-nav__icon-box" title="Info">
                         <svg className="header-nav__icon info-box">
                             <use xlinkHref="image/symbols.svg#icon-info" />
@@ -127,7 +127,7 @@ class Header extends React.Component<MyProps> {
                                 <p>Welcome to the Lizard Catalogue!</p>
                                 <p>
                                     This catalogue provides you a detailed overview of all the raster datasets in Lizard.
-                                    You're able to search for datasets, filter them per organisation, view detailed metadata and open datasets in the Lizard Portal and the Lizard API.
+                                    You can search for datasets, filter them per organisation, view detailed metadata and open datasets in the Lizard Portal and the Lizard API.
                                 </p>
                             </div>
                         </div>
