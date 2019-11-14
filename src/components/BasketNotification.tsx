df8b145d3e3ee75ccc3a196c5f7be4e560f673f9
Hoan Phung <hoan.phung@nelen-schuurmans.nl>
2019-11-14 14:55:06 +0100

    Refactor the basket and update its notification message (#73)
    * Refactor the basket to a separate component
    
    * Refactor information box into a separate component
    
    * Refactor add to basket notification to a separate component
    
    * Update notification message after adding a raster/wms layer

---
+++ src/components/BasketNotification.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './styles/BasketNotification.css';
+
+export default class BasketNotification extends React.Component {
+    render() {
+        return (
+            <div className="notification">
+                <p>Your selection has been updated successfully! Please go to the basket to see your selected item(s).</p>
+                {/* eslint-disable-next-line */}
+                <a href="#" className="notification-close">OK</a>
+            </div>
+        );
+    };
+};
