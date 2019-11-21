b85f996756c92fc7554b65d2abc306b6e0c0b59d
hoanphungt <hoanphung.cpo@gmail.com>
2019-11-21 09:45:42 +0100

    Show inbox messages in the inbox (export dropdown modal)

--- src/catalogue/components/ExportDropdown.tsx
+++ src/catalogue/components/ExportDropdown.tsx
@@ -1,34 +1,62 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { removeMessage, downloadMessage } from '../../action';
+import { Message } from '../../interface';
 import '../styles/ExportDropdown.css';
 
 interface MyProps {
-    toggleDropdowns: (event) => void
+    toggleDropdowns: (event) => void,
+    messages: Message[],
 };
 
-export default class ExportDropdown extends React.Component<MyProps> {
+interface PropsFromDispatch {
+    removeMessage: (id: string) => void,
+    downloadMessage: (id: string) => void,
+};
+
+class ExportDropdown extends React.Component<MyProps & PropsFromDispatch> {
+    removeMessage = (message: Message) => {
+        if (message.downloaded) {
+            fetch(`/api/v3/inbox/${message.id}/read/`, {
+                credentials: "same-origin",
+                method: 'POST'
+            });
+            this.props.removeMessage(message.id);
+        };
+    };
+
+    downloadFile = (message: Message) => {
+        if (message.url) {
+            this.props.downloadMessage(message.id);
+            return window.open(message.url);
+        };
+    };
+
     render() {
         return (
             <div className="export_dropdown" onMouseLeave={this.props.toggleDropdowns}>
-                <div className="export_dropdown-file">
-                    <div
-                        className="export_dropdown-filename"
-                        title={"NRR5min.zip (300mb)"}
-                    >
-                        NRR5min.zip (300mb)
+                {this.props.messages.map(message => (
+                    <div className="export_dropdown-file" key={message.id}>
+                        <div className="export_dropdown-filename" title={message.message}>
+                            {message.message}
+                        </div>
+                        <i 
+                            className={`fa ${message.url ? 'fa-download' : 'fa-spinner'}`}
+                            title={message.url ? 'download' : 'loading'}
+                            onClick={() => this.downloadFile(message)}
+                        />
+                        <div
+                            className="export_dropdown-close"
+                            title="remove file"
+                            style={{
+                                display: message.downloaded ? 'block' : 'none'
+                            }}
+                            onClick={() => this.removeMessage(message)}
+                        >
+                            &times;
+                        </div>
                     </div>
-                    <i className="fa fa-download" title="download" />
-                    &times;
-                </div>
-                <div className="export_dropdown-file">
-                    <div
-                        className="export_dropdown-filename"
-                        title={"NRR6min.zip (500mb)"}
-                    >
-                        NRR6min.zip (500mb)
-                    </div>
-                    <i className="fa fa-download" title="download" />
-                    &times;
-                </div>
+                ))}
                 <i className="export_dropdown-info">
                     When your files are ready, 
                     click on the download icon.
@@ -37,3 +65,10 @@ export default class ExportDropdown extends React.Component<MyProps> {
         );
     };
 };
+
+const mapDispatchToProps = (dispatch): PropsFromDispatch => ({
+    removeMessage: (id: string) => removeMessage(dispatch, id),
+    downloadMessage: (id: string) => downloadMessage(dispatch, id),
+});
+
+export default connect(null, mapDispatchToProps)(ExportDropdown);
