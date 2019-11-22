57d68ed063c03892bfbd8b4e1e082162c72d6a69
hoanphungt <hoanphung.cpo@gmail.com>
2019-11-22 11:13:27 +0100

    Show inbox messages in the inbox dropdown

--- src/catalogue/components/Inbox.tsx
+++ src/catalogue/components/Inbox.tsx
@@ -1,39 +1,104 @@
 import * as React from 'react';
 import '../styles/Inbox.css';
+import { Message } from '../../interface';
+import { connect } from 'react-redux';
+import { removeMessage, downloadFile } from '../../action';
+import { MyStore } from '../../reducers';
 
 interface MyProps {
+    inbox: Message[],
     closeAllDropdowns: () => void,
 };
 
-export default class Inbox extends React.Component<MyProps> {
+interface PropsFromState {
+    pendingExportTasks: number,
+};
+
+interface PropsFromDispatch {
+    removeMessage: (id: string) => void,
+    downloadFile: (id: string) => void,
+};
+
+type InboxProps = MyProps & PropsFromState & PropsFromDispatch;
+
+class Inbox extends React.Component<InboxProps> {
+    removeMessage = (message: Message) => {
+        fetch(`/api/v3/inbox/${message.id}/read/`, {
+            credentials: "same-origin",
+            method: 'POST'
+        });
+        this.props.removeMessage(message.id);
+    };
+
+    downloadFile = (message: Message) => {
+        window.open(message.url);
+        this.props.downloadFile(message.id);
+    };
+
     render() {
         return (
             <div className="inbox" onMouseLeave={this.props.closeAllDropdowns}>
-                <div className="inbox-file">
-                    <div
-                        className="inbox-filename"
-                        title={"NRR5min.zip (300mb)"}
-                    >
-                        NRR5min.zip (300mb)
+                {this.props.inbox.map(message => (
+                    <div className="inbox-file" key={message.id}>
+                        <div
+                            className="inbox-filename"
+                            title={message.message}
+                        >
+                            {message.message}
+                        </div>
+                        {message.url ? (
+                            <i
+                                className="fa fa-download inbox-download"
+                                title="download file"
+                                onClick={() => this.downloadFile(message)}
+                            />
+                        ) : null}
+                        {/* User can only remove the message if either the file
+                        has been downloaded or the export task failed */}
+                        {(message.downloaded || !message.url) ? (
+                            <div
+                                className="inbox-read"
+                                title="remove file"
+                                onClick={() => this.removeMessage(message)}
+                            >
+                                &times;
+                            </div>
+                        ) : null}
                     </div>
-                    <i className="fa fa-download" title="download" />
-                    &times;
-                </div>
-                <div className="inbox-file">
+                ))}
+                {this.props.pendingExportTasks ? (
                     <div
-                        className="inbox-filename"
-                        title={"NRR6min.zip (500mb)"}
+                        className="inbox-file inbox-pending-exports"
+                        title={`${this.props.pendingExportTasks} export(s) in progress`}
                     >
-                        NRR6min.zip (500mb)
+                        <div className="inbox-filename">
+                            {this.props.pendingExportTasks} export(s) in progress
+                        </div>
+                        <i className="fa fa-spinner inbox-progress" />
                     </div>
-                    <i className="fa fa-download" title="download" />
-                    &times;
-                </div>
-                <i className="inbox-info">
-                    When your files are ready, 
-                    click on the download icon.
-                </i>
+                ): null}
+                {(this.props.inbox.length === 0 && this.props.pendingExportTasks === 0) ? (
+                    <i className="inbox-info">
+                        No export tasks at the moment.
+                    </i>
+                ) : (
+                    <i className="inbox-info">
+                        When your files are ready, 
+                        click on the download icon.
+                    </i>
+                )}
             </div>
         );
     };
 };
+
+const mapStateToProps = (state: MyStore): PropsFromState => ({
+    pendingExportTasks: state.pendingExportTasks,
+});
+
+const mapDispatchToProps = (dispatch): PropsFromDispatch => ({
+    removeMessage: (id: string) => removeMessage(dispatch, id),
+    downloadFile: (id: string) => downloadFile(dispatch, id),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Inbox);
