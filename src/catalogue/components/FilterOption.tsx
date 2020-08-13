02ad65f5f2980126174deba1124718bac39dde1f
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-08-14 00:34:58 +0200

    Conver FilterOption.tsx from class based component to functional

--- src/catalogue/components/FilterOption.tsx
+++ src/catalogue/components/FilterOption.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { ObservationType, Organisation, Dataset } from '../../interface';
 import SearchBar from './SearchBar';
 import './FilterOption.css';
@@ -11,130 +11,110 @@ interface MyProps {
     removeItem: () => void,
 };
 
-interface MyState {
-    searchInput: string,
-    numberOfItems: number,
-    showList: boolean,
+// Helper function to get input value
+const getItemValue = (item: ObservationType | Organisation | Dataset, filterOption: MyProps['filterOption']) => {
+    if (filterOption === "observationType") {
+        return (item as ObservationType).parameter;
+    } else if (filterOption === "organisation") {
+        return (item as Organisation).name;
+    } else { // filterOption === "dataset"
+        return (item as Dataset).slug;
+    };
 };
 
-class FilterOption extends React.Component<MyProps, MyState> {
-    state: MyState = {
-        searchInput: '',
-        numberOfItems: 4,
-        showList: true,
-    };
+const FilterOption: React.FC<MyProps> = (props) => {
+    const {
+        filterOption,
+        listOfItems,
+        filterValue,
+        selectItem,
+        removeItem,
+    } = props;
 
-    // Helper function to get input value 
-    getItemValue = (item: ObservationType | Organisation | Dataset, filterOption: MyProps['filterOption']) => {
+    const [searchInput, setSearchInput] = useState('');
+    const [numberOfItems, setNumberOfItems] = useState(4);
+    const [showList, setShowList] = useState(true);
+
+    // Filter items using search input
+    const filteredListOfItems = listOfItems.filter(item => {
         if (filterOption === "observationType") {
-            return (item as ObservationType).parameter;
+            return (item as ObservationType).parameter.toLowerCase().includes(searchInput.toLowerCase());
         } else if (filterOption === "organisation") {
-            return (item as Organisation).name;
+            return (item as Organisation).name.toLowerCase().includes(searchInput.toLowerCase());
         } else { // filterOption === "dataset"
-            return (item as Dataset).slug;
+            return (item as Dataset).slug.toLowerCase().includes(searchInput.toLowerCase());
         };
+    });
+
+    let title: string;
+    if (filterOption === "observationType") {
+        title = "Observation Type";
+    } else if (filterOption === "organisation") {
+        title = "Organisation";
+    } else { // filterOption = "dataset"
+        title = "Dataset";
     };
 
-    render() {
-        const {
-            searchInput,
-            numberOfItems,
-            showList,
-        } = this.state;
-
-        const {
-            filterOption,
-            listOfItems,
-            filterValue,
-            selectItem,
-            removeItem,
-        } = this.props;
-
-        // Filter items using search input
-        const filteredListOfItems = listOfItems.filter(item => {
-            if (filterOption === "observationType") {
-                return (item as ObservationType).parameter.toLowerCase().includes(searchInput.toLowerCase());
-            } else if (filterOption === "organisation") {
-                return (item as Organisation).name.toLowerCase().includes(searchInput.toLowerCase());
-            } else { // filterOption === "dataset"
-                return (item as Dataset).slug.toLowerCase().includes(searchInput.toLowerCase());
-            };
-        });
-
-        let title: string;
-        if (filterOption === "observationType") {
-            title = "Observation Type";
-        } else if (filterOption === "organisation") {
-            title = "Organisation";
-        } else { // filterOption = "dataset"
-            title = "Dataset";
-        };
-
-        return (           
+    return (
+        <div
+            className="filter-option"
+            style={{
+                display: filteredListOfItems.length === 0 ? "none" : ""
+            }}
+        >
+            <div className="filter-title">
+                <h4>{title}</h4>
+                {showList ? (
+                    <button onClick={() => setShowList(false)}>
+                        <i className="fa fa-minus" />
+                    </button>
+                ) : (
+                    <button onClick={() => setShowList(true)}>
+                        <i className="fa fa-plus" />
+                    </button>
+                )}
+            </div>
             <div
-                className="filter-option"
+                className="filter-content"
                 style={{
-                    display: filteredListOfItems.length === 0 ? "none" : ""
+                    display: showList ? undefined : "none"
                 }}
             >
-                <div className="filter-title">
-                    <h4>{title}</h4>
-                    {showList ? (
-                        <button
-                            onClick={() => this.setState({showList: false})}
-                        >
-                            <i className="fa fa-minus" />
-                        </button>
-                    ) : (
-                        <button
-                            onClick={() => this.setState({showList: true})}
-                        >
-                            <i className="fa fa-plus" />
-                        </button>
-                    )}
-                </div>
-                <div
-                    className="filter-content"
-                    style={{
-                        display: showList ? undefined : "none"
-                    }}
-                >
-                    <SearchBar
-                        name="filterSearchBar"
-                        searchTerm={searchInput}
-                        title=""
-                        placeholder="search"
-                        onSearchSubmit={e => e.preventDefault()}
-                        onSearchChange={e => this.setState({ searchInput: e.currentTarget.value })}
-                    />
-                    {filterValue ? (
-                        <div className="filter-checked-item">
-                            <button onClick={removeItem}>x</button>
-                            {filterValue}
-                        </div>
-                    ) : (
-                        <div className="filter-checked-item" />
-                    )}
-                    <ul className="filter-list">
-                        {filteredListOfItems.slice(0, numberOfItems).map((item, i) => (
-                            <li className="filter-item" key={i}>
-                                <input
-                                    type="radio"
-                                    className="filter-radiobutton"
-                                    onChange={() => selectItem(this.getItemValue(item, filterOption))}
-                                    checked={filterValue === this.getItemValue(item, filterOption)}
-                                />
-                                <label className="filter-item-name">{this.getItemValue(item, filterOption)}</label>
-                            </li>
-                        ))}
-                        {numberOfItems < filteredListOfItems.length ? (
-                            <button className="filter-list-button" onClick={() => this.setState({ numberOfItems: numberOfItems + 4 })}>more ...</button>
-                        ) : null}
-                    </ul>
-                </div>
+                <SearchBar
+                    name="filterSearchBar"
+                    searchTerm={searchInput}
+                    title=""
+                    placeholder="search"
+                    onSearchSubmit={e => e.preventDefault()}
+                    onSearchChange={e => setSearchInput(e.currentTarget.value)}
+                />
+                {filterValue ? (
+                    <div className="filter-checked-item">
+                        <button onClick={removeItem}>x</button>
+                        {filterValue}
+                    </div>
+                ) : (
+                    <div className="filter-checked-item" />
+                )}
+                <ul className="filter-list">
+                    {filteredListOfItems.slice(0, numberOfItems).map((item, i) => (
+                        <li className="filter-item" key={i}>
+                            <input
+                                type="radio"
+                                className="filter-radiobutton"
+                                onChange={() => selectItem(getItemValue(item, filterOption))}
+                                checked={filterValue === getItemValue(item, filterOption)}
+                            />
+                            <label className="filter-item-name">{getItemValue(item, filterOption)}</label>
+                        </li>
+                    ))}
+                    {numberOfItems < filteredListOfItems.length ? (
+                        <button className="filter-list-button" onClick={() => setNumberOfItems(numberOfItems + 4)}>more ...</button>
+                    ) : null}
+                </ul>
             </div>
-        );
-    };
+        </div>
+    )
 };
 
 export default FilterOption;
