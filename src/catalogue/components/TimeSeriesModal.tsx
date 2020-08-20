a452dbc351c2eedcb85873335f0890fc6089c68c
Hoan Phung <hoanphung.ptso1@gmail.com>
2020-08-20 15:30:10 +0200

    Styling for timeseries selection modal

--- src/catalogue/components/TimeSeriesModal.tsx
+++ src/catalogue/components/TimeSeriesModal.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+import { mapBoxAccesToken } from "../../mapboxConfig.js";
 import '../styles/TimeSeriesModal.css';
 import '../styles/Modal.css';
 
@@ -15,17 +17,80 @@ const TimeSeriesModal: React.FC<MyProps> = (props) => {
 
     useEffect(() => {
         window.addEventListener('keydown', closeModalOnEsc);
-        return window.removeEventListener('keydown', closeModalOnEsc);
+        return () => window.removeEventListener('keydown', closeModalOnEsc);
     });
 
     return (
         <div className="modal-main modal-timeseries">
-            <div className="timeseries">
-                Timeseries selection modal
+            <div className="modal-header">
+                <span>Select Time-series</span>
+                <button onClick={props.toggleExportModal}>&times;</button>
             </div>
-
-            {/* eslint-disable-next-line */}
-            <a className="modal-close" onClick={props.toggleExportModal}>&times;</a>
+            <div className="timeseries">
+                <div className="timeseries-filter">
+                    <h3>1. SELECT LOCATIONS</h3>
+                    <span className="timeseries-helper-text">Select locations by clicking or select all locations with the same observation type</span>
+                    <div className="timeseries-map">
+                        <Map
+                            bounds={[[85, 180], [-85, -180]]}
+                            zoom={10}
+                        >
+                            <TileLayer url={`https://api.mapbox.com/styles/v1/nelenschuurmans/ck8sgpk8h25ql1io2ccnueuj6/tiles/256/{z}/{x}/{y}@2x?access_token=${mapBoxAccesToken}`} />
+                        </Map>
+                    </div>
+                    <h3>FILTER OBSERVATION TYPE</h3>
+                    <ul className="timeseries-observation-list">
+                        <li>
+                            <input type="checkbox"/>
+                            Temperature
+                        </li>
+                        <li>
+                            <input type="checkbox"/>
+                            Water
+                        </li>
+                    </ul>
+                </div>
+                <div className="timeseries-selection">
+                    <div className="timeseries-selected-locations">
+                        <h3>2. SELECTED LOCATIONS</h3>
+                        <span className="timeseries-helper-text">The selected locations will appear here</span>
+                        <ul className="timeseries-location-list">
+                            <li>Location 1 [code] [observation]</li>
+                            <li>Location 2 [code] [observation]</li>
+                        </ul>
+                    </div>
+                    <div className="timeseries-period">
+                        <h3>3. SELECT PERIOD</h3>
+                        <span className="timeseries-helper-text">Define a time period for your time-series</span>
+                        <div className="timeseries-period-container">
+                            <div className="timeseries-time-selection">
+                                <span>Start</span>
+                                <input type="text"/>
+                            </div>
+                            <div className="timeseries-time-selection">
+                                <span>End</span>
+                                <input type="text"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="timeseries-button-container">
+                        <div className="timeseries-buttons">
+                            <button className="button-action" title="Open in API">
+                                OPEN IN API
+                            </button>
+                            <button className="button-action" title="Open in Portal">
+                                OPEN IN PORTAL
+                            </button>
+                        </div>
+                        <div className="timeseries-buttons">
+                            <button className="button-action" style={{visibility: "hidden"}} />
+                            <button className="button-action" title="Export Time-series">
+                                EXPORT TIME-SERIES
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
