488cd7fb67bfbf33c8a46a871551a9c14a74797c
hoanphungt <hoanphung.cpo@gmail.com>
2021-01-05 15:46:27 +0100

    Remove service and version in getCapabilities request URL

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -78,11 +78,11 @@ export const openWMSDownloadURL = (wms: WMS) => {
 };
 
 export const getRasterGetCapabilitesURL = (raster: Raster) => {
-    return `${baseUrl}/wms/raster_${raster.uuid}/?request=GetCapabilities&service=wms&version=1.3.0`;
+    return `${baseUrl}/wms/raster_${raster.uuid}/?request=GetCapabilities`;
 };
 
 export const getDatasetGetCapabilitesURL = (dataset: Dataset) => {
-    return dataset && `${baseUrl}/wms/${dataset.slug}/?request=GetCapabilities&service=wms&version=1.3.0`;
+    return dataset && `${baseUrl}/wms/${dataset.slug}/?request=GetCapabilities`;
 };
 
 export const requestTimeseriesExport = (timeseriesUUIDs: string[][], start: number | null, end: number | null) => {
