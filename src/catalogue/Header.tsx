30930c1f27bb6ad4c67d297df06cc68957b78253
hoanphungt <hoanphung.cpo@gmail.com>
2021-03-08 11:16:27 +0100

    Refactor Header component

--- src/catalogue/Header.tsx
+++ src/catalogue/Header.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { MyStore, getRaster, getWMS } from '../reducers';
 import { Raster, Bootstrap, WMS, Message } from '../interface';
@@ -23,10 +23,15 @@ interface PropsFromState {
     inbox: Message[],
 };
 
-class Header extends React.Component<MyProps & PropsFromState> {
-    renderProfileDropdown(routeCurrentLocationInCatalog) {
+const Header: React.FC<MyProps & PropsFromState> = (props) => {
+    const { rasters, wmsLayers, user, inbox, openInboxDropdown, openProfileDropdown, closeAllDropdowns } = props;
+    const basket = [...rasters, ...wmsLayers];
+
+    const routeCurrentLocationInCatalog = window.location.href.split('?')[1] ? "?" + window.location.href.split('?')[1] : "";
+
+    const renderProfileDropdown = (routeCurrentLocationInCatalog: string) => {
         return (
-            <div className="user-profile_dropdown" onMouseLeave={this.props.closeAllDropdowns}>
+            <div className="user-profile_dropdown" onMouseLeave={props.closeAllDropdowns}>
                 {/* 
                 comment this out for ticket:
                 https://github.com/nens/lizard-catalogue/issues/252
@@ -47,80 +52,69 @@ class Header extends React.Component<MyProps & PropsFromState> {
         )
     };
 
-    render() {
-        const { rasters, wmsLayers, user, inbox, openInboxDropdown, openProfileDropdown, closeAllDropdowns } = this.props;
-        const basket = [...rasters, ...wmsLayers];
-
-        const routeCurrentLocationInCatalog = window.location.href.split('?')[1] ?  
-          "?" + window.location.href.split('?')[1]
-          :  
-          "";
-
-        return (
-            <nav className="header">
-                <div className="header-logo">
-                    <img src="image/lizard-logo-white.svg" alt="logo" className="header-logo__logo" onClick={() => window.location.href = ""} />
-                    <h3 className="header-logo__text">Catalogue</h3>
+    return (
+        <nav className="header">
+            <div className="header-logo">
+                <img src="image/lizard-logo-white.svg" alt="logo" className="header-logo__logo" onClick={() => window.location.href = ""} />
+                <h3 className="header-logo__text">Catalogue</h3>
+            </div>
+            <div className="header-nav">
+                <div className="header-nav__icon-box inbox-dropdown" onClick={openInboxDropdown}>
+                    <svg className="header-nav__icon">
+                        <use xlinkHref="image/symbols.svg#icon-download" />
+                    </svg>
+                    {inbox.length === 0 ? <span /> : <span className="header-nav__notification">{inbox.length}</span>}
+                    <span className="header-nav__inbox-text" style={{marginLeft: "1rem"}}>Export</span>
+                    {props.showInboxDropdown && (
+                        <Inbox
+                            inbox={inbox}
+                            closeAllDropdowns={closeAllDropdowns}
+                        />
+                    )}
                 </div>
-                <div className="header-nav">
-                    <div className="header-nav__icon-box inbox-dropdown" onClick={openInboxDropdown}>
+                <a href="#basket" className="header-nav__icon-box" title={`${basket.length } items in the basket`}>
+                    <svg className="header-nav__icon">
+                        <use xlinkHref="image/symbols.svg#icon-shopping-basket" />
+                    </svg>
+                    {basket.length === 0 ? <span /> : <span className="header-nav__notification">{basket.length}</span>}
+                    <span className="header-nav__text">Basket</span>
+                </a>
+                {user.authenticated ?
+                    <div className="header-nav__icon-box user-profile" onClick={openProfileDropdown}>
                         <svg className="header-nav__icon">
-                            <use xlinkHref="image/symbols.svg#icon-download" />
+                            <use xlinkHref="image/symbols.svg#icon-user" />
                         </svg>
-                        {inbox.length === 0 ? <span /> : <span className="header-nav__notification">{inbox.length}</span>}
-                        <span className="header-nav__inbox-text" style={{marginLeft: "1rem"}}>Export</span>
-                        {
-                        this.props.showInboxDropdown && (
-                            <Inbox
-                                inbox={inbox}
-                                closeAllDropdowns={closeAllDropdowns}
-                            />
-                        )}
+                        <span className="header-nav__text">{user.first_name}</span>
+                        {props.showProfileDropdown && renderProfileDropdown(routeCurrentLocationInCatalog)}
                     </div>
-                    <a href="#basket" className="header-nav__icon-box" title={`${basket.length } items in the basket`}>
+                    :
+                    <a href={`/accounts/login/?next=/catalogue/${encodeURIComponent(routeCurrentLocationInCatalog)}`} className="header-nav__icon-box user-profile">
                         <svg className="header-nav__icon">
-                            <use xlinkHref="image/symbols.svg#icon-shopping-basket" />
+                            <use xlinkHref="image/symbols.svg#icon-user" />
                         </svg>
-                        {basket.length === 0 ? <span /> : <span className="header-nav__notification">{basket.length}</span>}
-                        <span className="header-nav__text">Basket</span>
+                        <span className="header-nav__text">Login</span>
                     </a>
-                    {user.authenticated ?
-                        <div className="header-nav__icon-box user-profile" onClick={openProfileDropdown}>
-                            <svg className="header-nav__icon">
-                                <use xlinkHref="image/symbols.svg#icon-user" />
-                            </svg>
-                            <span className="header-nav__text">{user.first_name}</span>
-                            {this.props.showProfileDropdown && this.renderProfileDropdown(routeCurrentLocationInCatalog)}
-                        </div>
-                        :
-                        <a href={`/accounts/login/?next=/catalogue/${encodeURIComponent(routeCurrentLocationInCatalog)}`} className="header-nav__icon-box user-profile">
-                            <svg className="header-nav__icon">
-                                <use xlinkHref="image/symbols.svg#icon-user" />
-                            </svg>
-                            <span className="header-nav__text">Login</span>
-                        </a>
-                    }
-                    <a href="#information" className="header-nav__icon-box" title="Info">
-                        <svg className="header-nav__icon info-box">
-                            <use xlinkHref="image/symbols.svg#icon-info" />
-                        </svg>
-                    </a>
-                </div>
-                {/*This is the PopUp window when the basket is clicked*/}
-                <div className="modal" id="basket">
-                    <Basket
-                        rasters={rasters}
-                        wmsLayers={wmsLayers}
-                        basket={basket}
-                    />
-                </div>
-                {/*This is the PopUp window for the Information box*/}
-                <div className="modal" id="information">
-                    <Information />
-                </div>
-            </nav >
-        );
-    };
+                }
+                <a href="#information" className="header-nav__icon-box" title="Info">
+                    <svg className="header-nav__icon info-box">
+                        <use xlinkHref="image/symbols.svg#icon-info" />
+                    </svg>
+                </a>
+            </div>
+            {/*This is the PopUp window when the basket is clicked*/}
+            <div className="modal" id="basket">
+                <Basket
+                    rasters={rasters}
+                    wmsLayers={wmsLayers}
+                    basket={basket}
+                />
+            </div>
+            {/*This is the PopUp window for the Information box*/}
+            <div className="modal" id="information">
+                <Information />
+            </div>
+        </nav >
+    );
 };
 
 const mapStateToProps = (state: MyStore): PropsFromState => {
