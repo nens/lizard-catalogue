3ec9b0afb2000bd9424b677fe06d83005e42bdd2
hoanphungt <hoanphung.cpo@gmail.com>
2021-05-12 14:35:25 +0200

    Fix image not display issue

--- src/catalogue/components/Basket.tsx
+++ src/catalogue/components/Basket.tsx
@@ -4,6 +4,10 @@ import { Raster, WMS, LatLng } from '../../interface';
 import { getCenterPoint, zoomLevelCalculation, getBounds } from '../../utils/latLngZoomCalculation';
 import { openAllInLizard } from '../../utils/url';
 import { removeRasterFromBasket, removeWMSFromBasket } from '../../action';
+import rasterTemporalIcon from '../../images/raster-temporal.svg';
+import rasterNonTemporalIcon from '../../images/raster-non-temporal.svg';
+import wmsIcon from '../../images/wms.svg';
+import removeIcon from '../../images/remove_shopping_cart.svg';
 import '../styles/Basket.css';
 
 interface PropsFromDispatch {
@@ -61,31 +65,27 @@ class Basket extends React.Component<PropsFromDispatch & MyProps> {
                     {rasters.map(raster => (
                         <li className="basket_item" key={raster.uuid}>
                             {raster.temporal ?
-                                <img className="li li-type" src="image/raster-temporal.svg" alt="raster" /> :
-                                <img className="li li-type" src="image/raster-non-temporal.svg" alt="raster" />
+                                <img className="li li-type" src={rasterTemporalIcon} alt="temporal" /> :
+                                <img className="li li-type" src={rasterNonTemporalIcon} alt="non-temporal" />
                             }
                             <div className="li li-name">{raster.name}</div>
                             <div className="li li-org">{raster.organisation && raster.organisation.name}</div>
                             {/* <div className="li li-obs">{raster.observation_type && raster.observation_type.parameter}</div> */}
                             <div className="li li-time">{new Date(raster.last_modified).toLocaleDateString()}</div>
                             <div className="li li-basket li-basket__icon-box" onClick={() => removeRasterFromBasket(raster.uuid)}>
-                                <svg className="li-basket__icon">
-                                    <use xlinkHref="image/symbols.svg#icon-remove_shopping_cart" />
-                                </svg>
+                                <img className="li-basket__icon" src={removeIcon} alt="remove" />
                             </div>
                         </li>
                     ))}
                     {wmsLayers.map(wms => (
                         <li className="basket_item" key={wms.uuid}>
-                            <img className="li li-type" src="image/wms.svg" alt="wms" />
+                            <img className="li li-type" src={wmsIcon} alt="wms" />
                             <div className="li li-name">{wms.name}</div>
                             <div className="li li-org">{wms.organisation && wms.organisation.name}</div>
                             {/* <div className="li li-obs">{wms.slug}</div> */}
                             <div className="li li-time" />
                             <div className="li li-basket li-basket__icon-box" onClick={() => removeWMSFromBasket(wms.uuid)}>
-                                <svg className="li-basket__icon">
-                                    <use xlinkHref="image/symbols.svg#icon-remove_shopping_cart" />
-                                </svg>
+                                <img className="li-basket__icon" src={removeIcon} alt="remove" />   
                             </div>
                         </li>
                     ))}
