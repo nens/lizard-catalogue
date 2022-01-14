70ebf10e65cc808af6024af50cb0276034727122
hoanphungt <hoanphung.cpo@gmail.com>
2022-01-14 21:29:46 +0100

    Refactor Information component

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,33 +1,31 @@
-import * as React from 'react';
-import '../styles/Information.css';
+import React from 'react';
+import styles from './Information.module.css';
 
-export default class Information extends React.Component {
-    render() {
-        return (
-            <div className="information">
-                <div className="information_main">
-                    <h4>Information</h4>
-                    <div className="information_main-content">
-                        <p>Welcome to the Lizard Catalogue!</p>
-                        <p>
-                            This catalogue provides you a detailed overview of all the raster layercollections in Lizard.
-                            You can search for layer collections, filter them per organisation, view detailed metadata and open layercollections in the Lizard Portal and the Lizard API.
-                        </p>
-                    </div>
+export default function Information () {
+    return (
+        <div className={styles.InformationContainer} id={"information"}>
+            <div className={styles.InformationMain}>
+                <h4>Information</h4>
+                <div className={styles.InformationMainContent}>
+                    <p>Welcome to the Lizard Catalogue!</p>
+                    <p>
+                        This catalogue provides you a detailed overview of all the raster layercollections in Lizard.
+                        You can search for layer collections, filter them per organisation, view detailed metadata and open layercollections in the Lizard Portal and the Lizard API.
+                    </p>
                 </div>
-                <div className="information_footer">
-                    <a
-                        href="https://docs.lizard.net/e_catalog.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Documentation
-                    </a>
-                    <button onClick={() => window.location.href = '#'}>Close</button>
-                </div>
-                {/* eslint-disable-next-line */}
-                <a href="#" className="information_close">&times;</a>
             </div>
-        );
-    };
-};
+            <div className={styles.InformationFooter}>
+                <a
+                    href="https://docs.lizard.net/e_catalog.html"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Documentation
+                </a>
+                <button onClick={() => window.location.href = '#'}>Close</button>
+            </div>
+            {/* eslint-disable-next-line */}
+            <a href="#" className={styles.InformationClose}>&times;</a>
+        </div>
+    )
+}
