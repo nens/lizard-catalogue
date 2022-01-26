70a6c71cf5ad271446e9222487339a154de63941
hoanphungt <hoanphung.cpo@gmail.com>
2022-01-26 11:53:24 +0100

    Add css module to List components

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,8 @@
 import MDSpinner from 'react-md-spinner';
+import styles from '../styles/List.module.css';
 
 export default function LoadingScreen () {
   return (
-    <div className="list loading-screen"><MDSpinner size={50} /></div>
+    <div className={`${styles.List} ${styles.LoadingScreen}`}><MDSpinner size={50} /></div>
   )
 }
--- src/home/scenarios/ScenarioList.tsx
+++ src/home/scenarios/ScenarioList.tsx
@@ -8,7 +8,7 @@ import Pagination from '../../components/Pagination';
 import LoadingScreen from '../../components/LoadingScreen';
 import AccessModifier from '../../components/AccessModifier';
 import BasketNotification from '../../components/BasketNotification';
-import '../../styles/List.css';
+import styles from '../../styles/List.module.css';
 
 interface MyProps {
     searchTerm: string,
@@ -56,8 +56,8 @@ export default function ScenariosList (props: MyProps) {
     };
 
     return (
-        <div className="list">
-            <div className="list__top">
+        <div className={styles.List}>
+            <div className={styles.ListTop}>
                 <SearchBar
                     name="searchBar"
                     searchTerm={searchTerm}
@@ -66,49 +66,49 @@ export default function ScenariosList (props: MyProps) {
                     onSearchSubmit={onSearchSubmit}
                     onSearchChange={onSearchChange}
                 />
-                <div className="list__length">{count} scenarios</div>
+                <div className={styles.ListLength}>{count} scenarios</div>
             </div>
-            <div className="list__content">
-                <ul className="list__list">
-                    <li className="list__row-header">
-                        <div className="list__row list__row-box" />
-                        <div className="list__row list__row-name">
+            <div className={styles.ListContent}>
+                <ul className={styles.ListMainList}>
+                    <li className={styles.ListRowHeader}>
+                        <div className={`${styles.ListRow} ${styles.ListRowBox}`} />
+                        <div className={`${styles.ListRow} ${styles.ListRowName}`}>
                             Name
                             <i className="fa fa-sort" onClick={() => onSorting('name')} />
                         </div>
-                        <div className="list__row list__row-name">
+                        <div className={`${styles.ListRow} ${styles.ListRowName}`}>
                             Model name
                             <i className="fa fa-sort" onClick={() => onSorting('model_name')} />
                         </div>
-                        <div className="list__row list__row-name">
+                        <div className={`${styles.ListRow} ${styles.ListRowName}`}>
                             Username
                             <i className="fa fa-sort" onClick={() => onSorting('username')} />
                         </div>
-                        <div className="list__row list__row-org">
+                        <div className={`${styles.ListRow} ${styles.ListRowOrg}`}>
                             Organisation (owner)
                             <i className="fa fa-sort" onClick={() => onSorting('organisation__name')} />
                         </div>
-                        <div className="list__row list__row-time">
+                        <div className={`${styles.ListRow} ${styles.ListRowTime}`}>
                             Last update
                             <i className="fa fa-sort" onClick={() => onSorting('last_modified')} />
                         </div>
-                        <div className="list__row list__row-access" />
+                        <div className={`${styles.ListRow} ${styles.ListRowAccess}`} />
                     </li>
                     {scenarios.map((scenario: Scenario) => {
                         const checked = !!checkedScenarios.find(uuid => uuid === scenario.uuid);
                         return (
                             <li
-                                className={selectedItem === scenario.uuid ? "list__row-li list__row-li-selected" : "list__row-li"}
+                                className={selectedItem === scenario.uuid ? `${styles.ListRowLi} ${styles.ListRowLi__Selected}` : styles.ListRowLi}
                                 key={scenario.uuid}
                                 onClick={() => selectItem(scenario.uuid)}
                             >
-                                <input className="list__row list__row-normal list__row-box" type="checkbox" onChange={() => onCheckboxSelect(scenario.uuid)} checked={checked} />
-                                <div className="list__row list__row-normal list__row-name" title={scenario.name}>{scenario.name}</div>
-                                <div className="list__row list__row-normal list__row-name" title={scenario.model_name}>{scenario.model_name}</div>
-                                <div className="list__row list__row-normal list__row-name" title={scenario.username}>{scenario.username}</div>
-                                <div className="list__row list__row-normal list__row-org" title={scenario.organisation && scenario.organisation.name}>{scenario.organisation && scenario.organisation.name}</div>
-                                <div className="list__row list__row-normal list__row-time" title={scenario.last_modified}>{getLocalDateString(scenario.last_modified)}</div>
-                                <div className="list__row list__row-normal list__row-access">
+                                <input className={`${styles.ListRow} ${styles.ListRowNormal} ${styles.ListRowBox}`} type="checkbox" onChange={() => onCheckboxSelect(scenario.uuid)} checked={checked} />
+                                <div className={`${styles.ListRow} ${styles.ListRowNormal} ${styles.ListRowName}`} title={scenario.name}>{scenario.name}</div>
+                                <div className={`${styles.ListRow} ${styles.ListRowNormal} ${styles.ListRowName}`} title={scenario.model_name}>{scenario.model_name}</div>
+                                <div className={`${styles.ListRow} ${styles.ListRowNormal} ${styles.ListRowName}`} title={scenario.username}>{scenario.username}</div>
+                                <div className={`${styles.ListRow} ${styles.ListRowNormal} ${styles.ListRowOrg}`} title={scenario.organisation && scenario.organisation.name}>{scenario.organisation && scenario.organisation.name}</div>
+                                <div className={`${styles.ListRow} ${styles.ListRowNormal} ${styles.ListRowTime}`} title={scenario.last_modified}>{getLocalDateString(scenario.last_modified)}</div>
+                                <div className={`${styles.ListRow} ${styles.ListRowNormal} ${styles.ListRowAccess}`}>
                                     <AccessModifier accessModifier={scenario.access_modifier} />
                                 </div>
                             </li>
@@ -116,14 +116,14 @@ export default function ScenariosList (props: MyProps) {
                     })}
                 </ul>
             </div>
-            <div className="list__footer">
+            <div className={styles.ListFooter}>
                 <Pagination
                     count={count}
                     page={page}
                     onPageClick={onPageClick}
                 />
                 <button
-                    className="list__button-basket"
+                    className={styles.List__ButtonBasket}
                     disabled={checkedScenarios.length === 0 ? true : false}
                     onClick={addToBasket}
                 >
