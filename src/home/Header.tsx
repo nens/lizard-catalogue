ef113f58dee877a7f81c0c50bcf4b2fc26dec4a1
hoanphungt <hoanphung.cpo@gmail.com>
2022-01-26 11:06:16 +0100

    Add css module for Header component

--- src/home/Header.tsx
+++ src/home/Header.tsx
@@ -10,6 +10,7 @@ import exportIcon from '../images/download.svg';
 import basketIcon from '../images/shopping-basket.svg';
 import userIcon from '../images/user.svg';
 import infoIcon from '../images/info.svg';
+import styles from './Header.module.css';
 import '../styles/Header.css';
 
 interface MyProps {
@@ -28,31 +29,31 @@ export default function Header (props: MyProps) {
     const routeCurrentLocationInCatalog = window.location.href.split('?')[1] ? "?" + window.location.href.split('?')[1] : "";
 
     return (
-        <nav className="header">
-            <div className="header-logo" title={"client-version: " +packageJson.version}>
+        <nav className={styles.Header}>
+            <div className={styles.HeaderLogo} title={"client-version: " +packageJson.version}>
                 <img src={lizardLogo} alt="logo" className="header-logo__logo" onClick={() => window.location.href = "/"} />
-                <h3 className="header-logo__text">Catalogue</h3>
+                <h3 className={styles.HeaderLogoText}>Catalogue</h3>
             </div>
-            <div className="header-nav">
-                <a href="/" className="header-nav__icon-box">
+            <div className={styles.HeaderNav}>
+                <a href="/" className={styles.HeaderNav__IconBox}>
                     <i className="fa fa-home" style={{fontSize: "20px", }} />
-                    <span className="header-nav__text">Home</span>
+                    <span className={styles.HeaderNav__Text}>Home</span>
                 </a>
-                <div className="header-nav__icon-box inbox-dropdown" onClick={props.openInboxDropdown}>
-                    <img src={exportIcon} alt="export" className="header-nav__icon" />
-                    {inbox.length === 0 ? <span /> : <span className="header-nav__notification">{inbox.length}</span>}
-                    <span className="header-nav__inbox-text" style={{marginLeft: "1rem"}}>Export</span>
+                <div className={`${styles.HeaderNav__IconBox} ${styles.InboxDropdown}`} onClick={props.openInboxDropdown}>
+                    <img src={exportIcon} alt="export" className={styles.HeaderNav__Icon} />
+                    {inbox.length === 0 ? <span /> : <span className={styles.HeaderNav__Notification}>{inbox.length}</span>}
+                    <span className={styles.HeaderNav__InboxText} style={{ marginLeft: 10 }}>Export</span>
                     {props.showInboxDropdown ? <Inbox inbox={inbox} /> : null}
                 </div>
-                <a href="#basket" className="header-nav__icon-box" title={`${numberOfItemsInBasket} items in the basket`}>
-                    <img src={basketIcon} alt="basket" className="header-nav__icon" />
-                    {numberOfItemsInBasket ? <span className="header-nav__notification">{numberOfItemsInBasket}</span> : <span />}
-                    <span className="header-nav__text">Basket</span>
+                <a href="#basket" className={styles.HeaderNav__IconBox} title={`${numberOfItemsInBasket} items in the basket`}>
+                    <img src={basketIcon} alt="basket" className={styles.HeaderNav__Icon} />
+                    {numberOfItemsInBasket ? <span className={styles.HeaderNav__Notification}>{numberOfItemsInBasket}</span> : <span />}
+                    <span className={styles.HeaderNav__Text}>Basket</span>
                 </a>
                 {user.authenticated ? (
-                    <div className="header-nav__icon-box user-profile" onClick={props.openProfileDropdown}>
-                        <img src={userIcon} alt="user" className="header-nav__icon" />
-                        <span className="header-nav__text">{user.first_name}</span>
+                    <div className={`${styles.HeaderNav__IconBox} ${styles.UserProfile}`} onClick={props.openProfileDropdown}>
+                        <img src={userIcon} alt="user" className={styles.HeaderNav__Icon} />
+                        <span className={styles.HeaderNav__Text}>{user.first_name}</span>
                         {props.showProfileDropdown ? (
                             <ProfileDropdown
                                 routeCurrentLocationInCatalog={routeCurrentLocationInCatalog}
@@ -61,13 +62,13 @@ export default function Header (props: MyProps) {
                         ) : null}
                     </div>
                 ) : (
-                    <a href={`/accounts/login/?next=/catalogue/${encodeURIComponent(routeCurrentLocationInCatalog)}`} className="header-nav__icon-box user-profile">
-                        <img src={userIcon} alt="user" className="header-nav__icon" />
-                        <span className="header-nav__text">Login</span>
+                    <a href={`/accounts/login/?next=/catalogue/${encodeURIComponent(routeCurrentLocationInCatalog)}`} className={`${styles.HeaderNav__IconBox} ${styles.UserProfile}`}>
+                        <img src={userIcon} alt="user" className={styles.HeaderNav__Icon} />
+                        <span className={styles.HeaderNav__Text}>Login</span>
                     </a>
                 )}
-                <a href="#information" className="header-nav__icon-box" title="Info">
-                    <img src={infoIcon} alt="info" className="header-nav__icon" />
+                <a href="#information" className={styles.HeaderNav__IconBox} title="Info">
+                    <img src={infoIcon} alt="info" className={styles.HeaderNav__Icon} />
                 </a>
             </div>
             {/*This is the PopUp window when the basket is clicked*/}
